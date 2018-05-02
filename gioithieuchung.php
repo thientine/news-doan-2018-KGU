3b2ca8927802bab569933b1ebff28423c6f14678
thientine <vttien8392@gmail.com>
2018-05-02 20:31:58 +0700

    Add files via upload

---
+++ gioithieuchung.php
@@ -0,0 +1,5 @@
+	<?php 
+		include("menutop.php");	
+		include("center.php");	
+		include("bottom.php");	
+	?>	
---
+++ index.php
@@ -0,0 +1,6 @@
+	<?php 
+		echo "<link rel='shortcut icon' type='image/jpg' href='IMG/huyhieudoan.jpg'/>";
+		include("menutop.php");	
+		include("center.php");	
+		include("bottom.php");	
+	?>	
